a30bca1255ee6f58cbc035fd601c5baa28b34294
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2024-04-08 06:50:25 -0400

    Time: 36 ms (59.13%), Space: 16.5 MB (39.88%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,12 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        # Iterate through the digits list in reverse order
+        for i in reversed(range(len(digits))):
+            # If the current digit is not 9, simply add 1 to it
+            if digits[i] != 9:
+                digits[i] += 1
+                return digits  # Return the updated digits
+            # If the current digit is 9, set it to 0 and continue
+            digits[i] = 0
+        # If all digits are 9, append 1 to the beginning of the list
+        return [1] + digits
