7d1a0f97fff086c566bf48898b1d78a7574075bc
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2024-04-10 20:19:19 -0400

    Time: 99 ms (96.24%), Space: 19 MB (72.96%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        # Initialize the answer variable to store the single number
+       
+       # Initialize the answer variable to store the single number
         answer = 0
 
         # Iterate through each number in the input list
@@ -14,6 +15,31 @@ class Solution:
         return answer
 
 # XOR logic(same nums cancells out) 
-
 # 2^2^1 = 1
 # 4^1^2^1^2 = 4
+# O(n) time and O(1) space complexity
+
+# Example1: 
+# 0^2=2
+# 2^2=0
+# 0^1=1
+
+# You might have a question, what if the digits aren't sorted/next to each other? 
+#The result is same because Xor is Commutative and associative.
+
+#Note
+#Any number Xor'd with 0 will result the same number. Ex: 0^2 = 2
+#What is a bitwise operation? In bitwise operation, the integers are first converted into binary 
+#and then operations are performed according to the type of operation. In this problem we use Xor operation. 
+
+# Xor property aka exclusive OR
+# if both bits are 0 or both are 1, put 0 else put 1.
+
+# Xor general Example:
+
+#     1 1 0
+# XOR 1 0 0
+# ----------
+#     0 1 0
+
+
