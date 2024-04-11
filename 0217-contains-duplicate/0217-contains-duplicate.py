37864b36e1d5ac0bc4bf6d65fb2abe2dece382f0
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2024-04-11 19:43:11 -0400

    Time: 399 ms (97.43%), Space: 32 MB (32.05%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,15 +1,16 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        nums.sort()
-        num_set = set()
+        # nums.sort()
+        num_set = set()  # Create an empty set called num_set
 
-        for i in nums:
-            if i in num_set:
+        for num in nums:
+            if num in num_set:  # check if num is in num_set
                 return True
-            else:
-                num_set.add(i)
+            else:  # if num is not in num_set, add num to num_set
+                num_set.add(num)  
+        # If the for loop completes without finding duplicates, return False.
         return False
 
 
-# Create a hash set called num_set
-# [1,2,3,1]
+# In python hashsets are called sets.
+# We use sets because they can be used to store unique items.
