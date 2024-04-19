de6a19780198b06892b0f78d176423a4259036da
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2024-04-19 16:56:31 -0400

    Time: 45 ms (34.46%), Space: 16.5 MB (41.96%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        # Initialize left and right pointers for binary search
+        left = 0
+        right = x
+        #Perform binary search
+        while left <= right:
+            mid = (left + right) // 2
+            # If square of mid is less than x, update left pointer
+            if mid * mid < x:
+                left = mid + 1
+            # If square of mid is greater than x, update right pointer
+            elif mid * mid > x:
+                right = mid - 1
+            else:  # if mid*mid = x
+                return mid
+# If exact square root not found, return integer part of square root.
+#if the exact square root isn't found, 
+#returning right ensures that the function returns the 
+#closest integer that is less than or equal to the square root of x.
+        return right
