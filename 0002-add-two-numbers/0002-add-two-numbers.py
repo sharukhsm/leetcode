2eadc89aaac48747aafb8852e1f4bcf5fd21f33f
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2024-04-22 22:04:23 -0400

    Time: 55 ms (56.45%), Space: 16.7 MB (48.79%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Create a dummy node as the head of the result linked list
+        head = ListNode()
+        current = head   # Set current node to the head
+        carry = 0
+
+        # Loop until both linked lists and carry are empty
+        while l1 or l2 or carry:
+            #If l1 or l2 exist initialize it's value if no initialize 0.
+            l1_value = l1.val if l1 else 0
+            l2_value = l2.val if l2 else 0
+            # Calculate the total sum of current digits and carry
+            total = l1_value + l2_value + carry
+            # Create a new node with the remainder of the total sum
+            current.next = ListNode(total % 10)
+            # Update carry for the next iteration
+            carry = total // 10
+
+            #Move list pointers forward, if available
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            # Move current pointer to the next node
+            current = current.next 
+        # Return the next node after the dummy node (head), which is the actual result
+        return head.next
+
+        #Note 
+        # we do this l1_value = l1.val if l1 else 0, 465 + 3427 in these cases 
+        # because one of the list might have additional digits. 
