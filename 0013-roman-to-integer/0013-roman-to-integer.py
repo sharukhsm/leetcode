ae839b80f9b19b0e74f2e77eba7861e39454d21c
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2024-04-27 00:02:12 -0400

    Time: 39 ms (89.23%), Space: 16.8 MB (5.39%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -20,6 +20,10 @@ class Solution:
                 total+= roman[s[i]]
         return total + roman[s[-1]] #We know that we are left with the last character, so just add it to total.
                  
+
+#Roman numerals are usually written from Largest to smallerst and can just be added up.
+#Exception: if a nummer is smaller than the one after it, then you subtract the smaller one.
+
 # Logic:
 #Check if the current char is less than the next character subtract the current char from the total
 # If the current char is greater than the next character add the curr char to the total.
@@ -29,3 +33,13 @@ class Solution:
 # if the loop iterated until the last character, 
 # when it tries to access s[i+1] for the last character s[-1], 
 # it would result in an "index out of range" error.
+
+# Examples:
+# Roman numeral: "IX"
+# "I" (1) is smaller than "X" (10) which follows it. According to the exception rule, we subtract "I" from "X".
+# So, the total value is 10 - 1 = 9.
+
+# Roman numeral: "XXIV"
+# "X" (10) is followed by "X" (10), so we simply add their values.
+# Then, "I" (1) is smaller than "V" (5), so we subtract "I" from total which is 19. Remember we just add the last char to the totalWe jus add the last char to toal.
+# So, the total value is 10 + 10 -1 + 5 = 24.
