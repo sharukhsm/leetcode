4075437b9376fb4ed4494e942c0d36ec5f3dfb0d
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2024-04-28 12:26:13 -0400

    Time: 42 ms (52.68%), Space: 16.9 MB (7.26%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -0,0 +1,30 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        # Create a Counter object to count occurrences and store it in a variable called counter.
+        counter = Counter(arr) #{num: frequency}
+        
+        # Create an empty set to store unique frequencies
+        s = set()
+        
+        for num in counter.values():  # Iterate over the frequencies of elements in counter
+            # If the current frequency v is already in set s,
+            # it means there are duplicate occurrences of the same frequency
+            if num in s:
+                return False
+            else:             # Otherwise, add the current frequency v to set s
+                s.add(num)
+        #If the loop is complete that means there are no duplicates and we return true.
+        return True
+
+
+# Python Counter class is part of Collections module. Counter is a subclass of Dictionary 
+# and it's used to keep track of elements and their count.
+# Python Counter takes in input a list, tuple, dictionary, string, 
+# which are all iterable objects, and it will give you output that 
+# will have the count of each element in {key:value} pairs.
+
+# Example 1:
+# counter = Counter(arr) 
+# ['1': 3,'2': 2,'3': 1]
+
+#Fun fact: this is an apple coding question
