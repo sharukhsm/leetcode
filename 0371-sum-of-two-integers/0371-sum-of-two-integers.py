294959ea35bcff4a7d4f19c22b06c9e87bb895ac
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2024-05-04 16:34:46 -0400

    Time: 26 ms (95.09%), Space: 16.6 MB (6.82%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -1,9 +1,11 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        mask = 0xffffffff # (python default int size is not 32bit, it is very large 
+        
+        # (python default int size is not 32bit, it is very large 
         # number, so to prevent overflow and stop running into infinite loop, 
-        # we use 32bit mask to limit int size to 32bit
-
+        # we use 32bit mask to limit int size to 32bit)
+        mask = 0xffffffff 
+        
         # Iterate until there is no carry left, b holds the carry value.
         while (b & mask > 0):
             # Calculate the carry and Left shift by 1
