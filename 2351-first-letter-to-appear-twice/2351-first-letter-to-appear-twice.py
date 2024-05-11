690aa590bf8d5ef8172432808bf85cd7305294ae
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2024-05-11 12:29:43 -0400

    Time: 30 ms (84.93%), Space: 16.5 MB (83.08%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -1,24 +1,28 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
+        
         # Using a set: Time: O(n), space O(n)
+        
         seen = set()  # Space complexity: O(n)
         for i in s:  # Time complexity: O(n)
             if i in seen:  # Time complexity: O(1)
                 return i  # Time complexity: O(1)
             else:
                 seen.add(i)  # Time complexity: O(1)
-        # Overall time complexity: O(n)
+
 
         # Using a list: Time: O(n^2), space O(n)
+        
         # seen = []  # Space complexity: O(n)
         # for i in s:  # Time complexity: O(n)
         #     if i in seen:  # Time complexity: O(n)
         #         return i  # Time complexity: O(1)
         #     else:
         #         seen.append(i)  # Time complexity: O(1)
-        # # Overall time complexity: O(n^2)
+
 
 # Using a list has a time complexity of O(n^2) due to the linear search (i in seen).
 # Therefore, the implementation using a set is better in terms of time complexity.
+# Both has same space complexity: O(n)
 # If the character set expands in the future to include things like upper case, symbols, accented characters, etc. 
 # then the list becomes less effective. So set is future proof.
