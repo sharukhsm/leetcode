081afc6933a143084058adc61ba0efb38f611d3e
Sharukhsm <41145228+sharukhsm@users.noreply.github.com>
2024-05-11 12:43:28 -0400

    Time: 40 ms (17.51%), Space: 16.5 MB (28.38%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.py
@@ -14,15 +14,25 @@ class Solution:
         # Using a list: Time: O(n^2), space O(n)
         
         # seen = []  # Space complexity: O(n)
-        # for i in s:  # Time complexity: O(n)
+        # for i in s:  # Time complexity: O(n). The in operator on a Python  list is O(n)
         #     if i in seen:  # Time complexity: O(n)
         #         return i  # Time complexity: O(1)
         #     else:
         #         seen.append(i)  # Time complexity: O(1)
 
+        
+        # Using a dictionary: Time: O(n), space O(n)
+        
+        seen = {}  # Space complexity: O(n)
+        for i in s:  # Time complexity: O(n)
+            if i in seen:  # Time complexity: O(1) average case
+                return i  # Time complexity: O(1)
+            else:
+                seen[i] = True  # Time complexity: O(1)
 
-# Using a list has a time complexity of O(n^2) due to the linear search (i in seen).
+# The time complexity of using a list is O(n^2) because the in operator on a Python list is O(n).
+# which performs a linear search, and has a time complexity of O(n).
 # Therefore, the implementation using a set is better in terms of time complexity.
 # Both has same space complexity: O(n)
 # If the character set expands in the future to include things like upper case, symbols, accented characters, etc. 
-# then the list becomes less effective. So set is future proof.
+# then the list becomes less effective. So using a set/dictionary is future proof.
